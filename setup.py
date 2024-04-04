3ad866c9b3ab32abdd8f689194f24a4fe133a320
vitaliy.vazhinskiy <vitaliy.vazhinskiy@megamarket.ru>
2024-04-04 15:45:57 +0300

    2024-04-04

--- setup.py
+++ setup.py
@@ -2,8 +2,25 @@ from setuptools import setup
 
 
 setup(name='vvm_lib',
-      version='0.02',
+      version='0.03',
       description='my frequently used functions',
-      packages=['vvm_lib'],
+      packages=[
+          'vvm_lib'
+          ],
       author_email='v.vazhinskiy@yandex.ru',
-      zip_safe=False)
+      author="vvazhinskiy",
+      url="https://github.com/VazhikVM/vvm_lib",
+      zip_safe=False,
+      install_requires=[
+          "pandas",
+          "numpy",
+          "psycopg2-binary",
+          "hvac",
+          "openpyxl",
+          "requests-ntlm",
+          "oauth2client",
+          "gspread",
+          "pymssql",
+          ],
+      
+      )
